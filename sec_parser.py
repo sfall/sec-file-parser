31077689ee0d50d5a026e50678b0ba45bdf13476
sfall <sfall@my.tnstate.edu>
2015-09-16 22:32:23 -0500

    Classify + Fix to reflect nesting issues

--- sec_parser.py
+++ sec_parser.py
@@ -11,48 +11,84 @@ import json
 import re
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString
+from sys import argv
 
-# Two steps to preprocess the file:
-# 1) Style information takes up the majority of the text in the file; it can be
-#    removed using regular expressions before further processing takes place
-# 2) <br> tags will be replaced with '\n' so that text may be more uniformly
-#    extracted
-with open('BMI-2013.12.31-10K.html') as f:
-    html1 = re.sub(r' style=".*?"', '', f.read())
-    html2 = re.sub(r'<br>', '\n', html1)
-    soup = BeautifulSoup(html2)
 
-body = soup.find('body')
-# The plan for 'document.txt' is to iterate through the children tags in
-# the body; getting the text content from them should be straightforward
-with open('document.txt', 'w') as f1:
-    for tag in body.children:
-        text = str(tag) if isinstance(tag, NavigableString) else tag.get_text()
-        if not text.endswith('\n'):
-            text += '\n'
-        f1.write(text)
-# The tables don't look good, and some lines don't appear to have
-# had line breaks appended to the end, but I'll look into
-# 'paragraphs.txt' first
-with open('document.txt') as f1:
-    document_txt = f1.read()
-paragraphs = []
-search_chars = 20
-for tag in body.children:
-    # Exclude '$' found in tables
-    if tag.find('table'):
-        continue
-    text = str(tag) if isinstance(tag, NavigableString) else tag.get_text()
-    if '$' in text:
-        start_index = document_txt.index(text[:search_chars])
-        end_index = start_index + len(text)
-        paragraphs.append({
-            'text': text,
-            'start': start_index,
-            'end': end_index
-        })
-with open('paragraphs.txt', 'w') as f2:
-    json.dump(paragraphs, f2)
-# I wrote this script on a Windows machine, so the indexes seemed off.
-# Once I set my text reader (Notepad++) to use '\n' instead '\r\n',
-# the indexes were correct again.
+class SECParser(object):
+    def __init__(self, file):
+        self.text = file.read()
+        self.soup = None
+        self.search_chars = 20
+
+    def preprocess(self):
+        """
+        Two steps to preprocess the file:
+        1) Style information takes up the majority of the text in the file; it can be
+           removed using regular expressions before further processing takes place
+        2) <br> tags will be replaced with '\n' so that text may be more uniformly
+           extracted
+        """
+        html1 = re.sub(r' style=".*?"', '', self.text)
+        html2 = re.sub(r'<br>', '\n', html1)
+        self.soup = BeautifulSoup(html2)
+
+    def generate_document(self):
+        """
+        The plan for 'document.txt' is to iterate through the children tags in
+        the body; getting the text content from them should be straightforward
+        """
+        body = self.soup.find('body')
+        with open('document.txt', 'wb') as f1:
+            for tag in body.children:
+                text = (str(tag)
+                        if isinstance(tag, NavigableString)
+                        else tag.get_text())
+                if not text.endswith('\n'):
+                    text += '\n'
+                f1.write(text.encode())
+
+    def generate_paragraphs(self):
+        """
+        Because of nesting issues, this function recursively searches
+        for paragraphs.
+        :return:
+        """
+        def dig(hr_tag, end_index):
+            paragraphs = []
+            for tag in hr_tag.children:
+                if tag.name == 'hr':
+                    return paragraphs + dig(tag, end_index)
+                text = (str(tag)
+                        if isinstance(tag, NavigableString)
+                        else tag.get_text())
+                if '$' in text and not tag.find('table'):
+                    start_index = document_txt.index(text[:search_chars])
+                    end_index = start_index + len(text)
+                    paragraphs.append({
+                        'text': text,
+                        'start': start_index,
+                        'end': end_index
+                    })
+            return paragraphs
+
+        with open('document.txt', 'rb') as f1:
+            document_txt = f1.read().decode()
+        search_chars = 20
+        paragraphs = dig(self.soup.find('body'), 0)
+        with open('paragraphs.txt', 'wb') as f2:
+            f2.write(json.dumps(paragraphs).encode())
+
+
+if __name__ == '__main__':
+    try:
+        filename = argv[1]
+    except IndexError:
+        print('Usage: python3 sec_parser.py <filename>')
+        exit(1)
+
+    with open(filename) as file:
+        parser = SECParser(file)
+
+    parser.preprocess()
+    parser.generate_document()
+    parser.generate_paragraphs()
